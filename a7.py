acd1e4591878827bf38ea8b968b0791b7efd3997
Patrick Nyman <pnyman@cps.edu>
2026-04-10 11:09:09 -0500

    update correct dictionaries

--- a7.py
+++ a7.py
@@ -69,8 +69,9 @@ class BayesClassifier:
         # enumerate function, which loops over something and has an automatic counter.
         # write something like this to track progress (note the `# type: ignore` comment
         # which tells mypy we know better and it shouldn't complain at us on this line):
+
         for index, filename in enumerate(files, 1): # type: ignore
-            print(f"Training on file {index} of {len(files)}")
+            # print(f"Training on file {index} of {len(files)}")
             # <the rest of your code for updating frequencies here>
             # print(f"{index}: {filename}")
             text = self.load_file(os.path.join(self.training_data_directory, filename))
@@ -89,10 +90,13 @@ class BayesClassifier:
         # positive frequency dictionary. If it is neither a postive or negative file,
         # ignore it and move to the next file (this is more just to be safe; we won't
         # test your code with neutral reviews)
+
             if filename.startswith(self.neg_file_prefix):
                 self.update_dict(tokens, self.neg_freqs)
             elif filename.startswith(self.pos_file_prefix):
                 self.update_dict(tokens, self.pos_freqs)
+        # print(self.pos_freqs)
+        print(self.neg_freqs)
         
         # Updating frequences: to update the frequencies for each file, you need to get
         # the text of the file, tokenize it, then update the appropriate dictionary for
@@ -243,15 +247,15 @@ class BayesClassifier:
 if __name__ == "__main__":
     # uncomment the below lines once you've implemented `train` & `classify`
     b = BayesClassifier()
-    # a_list_of_words = ["I", "really", "like", "this", "movie", ".", "I", "hope", \
-    #                    "you", "like", "it", "too"]
-    # a_dictionary = {}
-    # b.update_dict(a_list_of_words, a_dictionary)
-    # assert a_dictionary["I"] == 2, "update_dict test 1"
-    # assert a_dictionary["like"] == 2, "update_dict test 2"
-    # assert a_dictionary["really"] == 1, "update_dict test 3"
-    # assert a_dictionary["too"] == 1, "update_dict test 4"
-    # print("update_dict tests passed.")
+    a_list_of_words = ["I", "really", "like", "this", "movie", ".", "I", "hope", \
+                       "you", "like", "it", "too"]
+    a_dictionary = {}
+    b.update_dict(a_list_of_words, a_dictionary)
+    assert a_dictionary["I"] == 2, "update_dict test 1"
+    assert a_dictionary["like"] == 2, "update_dict test 2"
+    assert a_dictionary["really"] == 1, "update_dict test 3"
+    assert a_dictionary["too"] == 1, "update_dict test 4"
+    print("update_dict tests passed.")
 
     # pos_denominator = sum(b.pos_freqs.values())
     # neg_denominator = sum(b.neg_freqs.values())
